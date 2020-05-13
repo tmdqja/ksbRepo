2e621190e0b3d5ff6a175f63307a052cac467599
사용자 <@175.112.100.94>
2020-05-14 07:41:43 +0900

    정수형을 출력하는 프로그램

---
+++ ksbprj/src/ksbprj/정수형을출력하는프로그램.java
@@ -0,0 +1,19 @@
+package ksbprj;
+
+import java.util.Scanner;
+
+public class 정수형을출력하는프로그램 {
+
+	public static void main(String[] args) {
+		int data;
+		Scanner s = new Scanner(System.in);
+		System.out.print("정수를 입력하세요 ==>");
+		data = s.nextInt();
+		
+		System.out.printf("10진수 ==> %d \n " , data);
+		System.out.printf("16진수 ==> %X \n " , data);
+		System.out.printf("8진수 ==> %O \n " , data);
+
+	}
+
+}
