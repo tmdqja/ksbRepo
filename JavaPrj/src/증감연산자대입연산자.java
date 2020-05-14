59b40a31d14118ed1efbfc6ff0dc0baedfde5abe
ksb <tmdqja0906@naver.com>
2020-05-14 15:56:12 +0900

    증감연산자와 대입연산자

---
+++ JavaPrj/src/증감연산자대입연산자.java
@@ -0,0 +1,27 @@
+
+public class 증감연산자대입연산자 {
+
+	public static void main(String[] args) {
+		int a= 10;
+		
+		a++;
+		System.out.printf("a++ ==> %d \n" , a);
+		
+a--;
+System.out.printf("a-- ==> %d \n" , a);
+
+a+=5;
+System.out.printf("a += 5 ==> %d \n" , a);
+
+a-=5;
+System.out.printf("a -= 5 ==> %d \n" , a);
+
+a*=5;
+System.out.printf("a *= 5 ==> %d \n" , a);
+
+a/=5;
+System.out.printf("a /= 5 ==> %d \n" , a);
+
+	}
+
+}
