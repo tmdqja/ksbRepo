9ad5b13b35b13861c9ac1f80148705a231ce6c39
DATA8320-26 <DATA8320-26@DESKTOP-KU8SND7>
2020-05-14 15:10:29 +0900

    다시하기

---
+++ JavaPrj/src/변수에변수를대입2.java
@@ -0,0 +1,20 @@
+
+public class 변수에변수를대입2 {
+
+	public static void main(String[] args) {
+		int a,b;
+		float c,d;
+		
+		a=100;
+		b=a;
+		
+		c=111.1f;
+		d=c;
+		
+		System.out.printf("a,b의 값 ==> %d , %d\n",a,b);
+		System.out.printf("c,d의 값 ==> %5.1f , %5.1f \n" , c,d);
+		
+		
+	}
+
+}
