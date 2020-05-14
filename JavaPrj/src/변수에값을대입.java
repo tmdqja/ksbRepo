0a59f747187681cf9d0e12726e0a52eade27330b
ksb <tmdqja0906@naver.com>
2020-05-14 15:22:24 +0900

    변수에 값을 대입

---
+++ JavaPrj/src/변수에값을대입.java
@@ -0,0 +1,18 @@
+
+public class 변수에값을대입 {
+
+	public static void main(String[] args) {
+		int a;
+		float b;
+		
+		a = (int)123.45f;
+		
+		b= 200;
+		
+		System.out.printf("a의 값 ==> %d \n" , a);
+		System.out.printf("b의 값 ==> %f \n" , b);
+		
+
+	}
+
+}
