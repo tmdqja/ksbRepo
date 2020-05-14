f19ff14a8751e80dbb6e06bdb3da2e355057dc04
ksb <tmdqja0906@naver.com>
2020-05-14 16:20:38 +0900

    입력된 두 실수 계산

---
+++ JavaPrj/src/입력된두실수산수계산.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class 입력된두실수산수계산 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		float a,b;
+		float result;
+		
+		System.out.printf("첫번째 계산할 값을 입력하세요 ==> ");
+		a=s.nextFloat();
+		System.out.printf("두번째 계산할 값을 입력하세요 ==> ");
+		b=s.nextFloat();
+		
+		result = a+b;
+		System.out.printf(" %5.2f + %5.2f = 5.2f \n" , a,b,result);
+		
+		result = a-b;
+		System.out.printf(" %5.2f - %5.2f = 5.2f \n" , a,b,result);
+		
+		result = a*b;
+		System.out.printf(" %5.2f * %5.2f = 5.2f \n" , a,b,result);
+		
+		result = a/b;
+		System.out.printf(" %5.2f / %5.2f = 5.2f \n" , a,b,result);
+		
+		result = (int)a%(int)b;
+		System.out.printf(" %d %% %d = %d \n", (int)a, (int)b, (int)result);
+		
+	}
+
+}
