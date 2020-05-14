b65437f7ab92cae839364ee220f4dd7d7e5d60bf
ksb <tmdqja0906@naver.com>
2020-05-15 08:45:32 +0900

    기본for문

---
+++ JavaPrj/src/For.java
@@ -0,0 +1,13 @@
+
+public class For {
+
+	public static void main(String[] args) {
+		int i;
+		
+		for (i=5; i>0; i--) {
+			System.out.printf("%d : 안녕하세요 빙글빙글 for문을 공부중입니다.^^ \n ", i);
+		}
+
+	}
+
+}
