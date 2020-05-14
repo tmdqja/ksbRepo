809e5d5762800181a5136eea9a96cfe5f95b8c20
DATA8320-26 <DATA8320-26@DESKTOP-KU8SND7>
2020-05-14 15:07:44 +0900

    해보기

--- JavaPrj/src/숫자4개를더하는프로그램.java
+++ JavaPrj/src/숫자4개를더하는프로그램.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 
 public class 숫자4개를더하는프로그램 {
 
+	private static Scanner s;
+
 	public static void main(String[] args) {
 		int a,b,c,d;
 		int result;
 		
-		Scanner s = new Scanner(System.in);
+		s = new Scanner(System.in);
 		System.out.print("첫번째 계산할 값을 입력하세요 ==> ");
 		a = s.nextInt();
 		System.out.print("두번째 계산할 값을 입력하세요 ==> ");
--- JavaPrj/src/한번만.java
+++
@@ -1,9 +0,0 @@
-
-public class 한번만 {
-
-	public static void main(String[] args) {
-		System.out.println("잔디심기");
-
-	}
-
-}
